1d06db76b53e084166bdc80ba4251a64dd8e5e60
thomasabishop <tactonbishop@gmail.com>
2021-07-08 12:22:28 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'angular-learning-lab';
+  title = 'Angular Learning Lab';
+  headerColor = 'crimson';
 }
