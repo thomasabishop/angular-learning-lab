d039488c5f95a603d7009e2b4b8d757d72c49742
thomasabishop <tactonbishop@gmail.com>
2021-07-14 17:05:54 +0100

    Add demos of conditional styling

--- src/app/styling-conventions/styling-conventions.component.ts
+++ src/app/styling-conventions/styling-conventions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-styling-conventions',
@@ -6,8 +6,45 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./styling-conventions.component.less'],
 })
 export class StylingConventionsComponent implements OnInit {
-  darkRed = 'crimson'
   constructor() {}
-
   ngOnInit(): void {}
+
+  darkRed = 'crimson';
+  languages = ['json', 'typescript', 'html'];
+  codeOne = `const PEOPLE = [
+  {
+    name: 'Thomas',
+    age: 32,
+  },
+  {
+    name: 'Martha',
+    age: 23,
+  },
+  {
+    name: 'Mary Jo',
+    age: 62,
+  },
+];
+  `;
+
+  codeTwo = `applyMultipleStylesConditionally() {
+    if (this.people.age < 30)
+      return {
+        color: 'blue',
+        fontSize: '18px',
+      };
+  }
+  // Note that we use camelCase for the style properties, not CSS kebab-case`;
+
+  codeThree = ` <p [ngStyle]="applyMultipleStylesConditionally()">{{ people.age }}</p>`;
+
+  codeFour = `applyConditionalClass() {
+    return {
+      oldest: this.people.age > 50,
+    };
+   }`;
+
+  codeFive = `applyConditionalClassAlternative() {
+    return this.people.age > 50 && ['oldest'];
+  }`;
 }
