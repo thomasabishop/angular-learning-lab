62e86353cdf595a432e820f81d89d7013e20ab56
thomasabishop <tactonbishop@gmail.com>
2021-08-04 11:26:59 +0100

    Reduce repetition in test by invoking beforeEach

--- src/app/learning-testing/class-only-component-test/class-only-component-test.component.spec.ts
+++ src/app/learning-testing/class-only-component-test/class-only-component-test.component.spec.ts
@@ -1,13 +1,21 @@
+import {TestBed} from '@angular/core/testing';
 import {ClassOnlyComponentTestComponent} from './class-only-component-test.component';
 
 describe('Component class-only tests', () => {
+  let component: ClassOnlyComponentTestComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ClassOnlyComponentTestComponent],
+    });
+    component = TestBed.inject(ClassOnlyComponentTestComponent);
+  });
+
   it('should start with mood as happy', () => {
-    const comp = new ClassOnlyComponentTestComponent();
-    expect(comp.mood).toMatch('happy');
+    expect(component.mood).toMatch('happy');
   });
   it('mood should change to sad on click', () => {
-    const comp = new ClassOnlyComponentTestComponent();
-    comp.toggleMood();
-    expect(comp.mood).toMatch('sad');
+    component.toggleMood();
+    expect(component.mood).toMatch('sad');
   });
 });
