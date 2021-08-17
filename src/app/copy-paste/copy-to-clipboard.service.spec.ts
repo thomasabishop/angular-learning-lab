433e14a99c16f924d66f17dcbc7699c57e90c503
thomasabishop <tactonbishop@gmail.com>
2021-08-17 13:46:57 +0100

    Complete clipboard service and test.

--- src/app/copy-paste/copy-to-clipboard.service.spec.ts
+++ src/app/copy-paste/copy-to-clipboard.service.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
-
-import { CopyToClipboardService } from './copy-to-clipboard.service';
+import {CopyToClipboardService} from './copy-to-clipboard.service';
 
 describe('CopyToClipboardService', () => {
   let service: CopyToClipboardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CopyToClipboardService);
+    // TestBed.configureTestingModule({});
+    // service = TestBed.inject(CopyToClipboardService);
+    service = new CopyToClipboardService();
+    spyOn(document, 'addEventListener').and.callThrough();
+    spyOn(document, 'execCommand').and.callThrough();
+    spyOn(document, 'removeEventListener').and.callThrough();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('it should copy `innerText` from element to clipboard', () => {
+    const fakeElement = document.createElement('div');
+    fakeElement.innerText = 'Fake element inner text';
+    service.writeToClipboard(fakeElement);
+    expect(document.addEventListener).toHaveBeenCalledWith('copy', jasmine.any(Function));
+    expect(document.execCommand).toHaveBeenCalledWith('copy');
+    expect(document.removeEventListener).toHaveBeenCalledWith('copy', jasmine.any(Function));
+  });
+
+  it('it should copy `innerHTML` from element to clipboard', () => {
+    const fakeElement = document.createElement('div');
+    fakeElement.innerHTML = `<p>Fake element inner HTML</p>`;
+    service.writeToClipboard(fakeElement);
+    expect(document.addEventListener).toHaveBeenCalledWith('copy', jasmine.any(Function));
+    expect(document.execCommand).toHaveBeenCalledWith('copy');
+    expect(document.removeEventListener).toHaveBeenCalledWith('copy', jasmine.any(Function));
   });
 });
--- src/app/copy-paste/copy-to-clipboard.service.ts
+++ src/app/copy-paste/copy-to-clipboard.service.ts
@@ -7,24 +7,18 @@ export class CopyToClipboardService {
   /**
    * Writes string to clipboard
    * @remarks
-   * The modern Clipboard API method is preferred but fallback is provided.
-   * @param targetText  - The string to be written
+   * @param targetElement  - The element to be written
    */
 
-  public writeToClipboard(targetText: string): void {
-    if (!navigator.clipboard) {
-      this.writeToClipboardFallback(targetText);
-    } else {
-      navigator.clipboard.writeText(targetText);
-    }
-  }
-
-  private writeToClipboardFallback(targetText: string): void {
+  public writeToClipboard(targetElement: HTMLElement): void {
     const copyHandler = (event: ClipboardEvent) => {
       event.preventDefault();
-      event.clipboardData!.setData('text/plain', targetText);
+      // Allow for cross-pasting in different user editor contexts: rich-text, plaintext
+      event.clipboardData!.setData('text/plain', targetElement.innerText);
+      event.clipboardData!.setData('text/html', targetElement.innerHTML);
     };
     document.addEventListener('copy', copyHandler);
     document.execCommand('copy');
+    document.removeEventListener('copy', copyHandler);
   }
 }
